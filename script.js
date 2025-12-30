cce939fe0b225ec5c80fc21b46c999afb48f597d
St4m <117040208+St4m@users.noreply.github.com>
2025-12-30 22:28:23 +0530

    Enhance openLetter function with sound and aria attribute
    Added sound playback when opening the letter and updated aria attribute for accessibility.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function openLetter() {
+  const envelope = document.querySelector('.envelope');
+  envelope.classList.toggle('open');
+  envelope.setAttribute(
+    'aria-expanded',
+    envelope.classList.contains('open')
+  );
+}
+const sound = new Audio('Myktybek-Nurlanbekov-Kyyalymsy-ZHa-y-yr-2022.mp3');
+
+function openLetter() {
+  sound.play();
   document.querySelector('.envelope').classList.toggle('open');
 }
